a25c34af389f92b4c47678532ca34fc8edbfa43a
Philip Owusu Asare <philip.asare@amalitech.org>
2022-05-13 09:46:11 +0000

    Adding another page

---
+++ pages/_.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useRoute } from '@nuxtjs/composition-api'
+import { useStoryblok } from '@storyblok/nuxt'
+
+const route = useRoute()
+const slug = route.value.params.pathMatch
+
+const { story, fetchState } = useStoryblok(slug ? slug : 'home', {
+  version: 'draft',
+})
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
